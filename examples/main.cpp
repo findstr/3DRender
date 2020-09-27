d6dd8c5a31d23a266ee2f2693f10a298dbcfb03e
findstr <findstr@sina.com>
2020-09-27 18:04:33 +0800

    main add show window

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,15 +9,26 @@
 
 int main()
 {
+	int key = 0;
 	scene scene1;
 	screen scrn(800, 800);
 	pathtracer pr;
 	raytracer rr;
 	scene_loader sl;
 	sl.load(scene1, "scene/whitted.scene");
-	scrn.clear();
-	rr.render(scene1, scrn);
-	scrn.dump("out.ppm");
+	while(key != 27)
+	{
+		scrn.clear();
+		rr.render(scene1, scrn);
+		scrn.show();
+		key = cv::waitKey(10);
+		if (key == 'a' )
+		{
+		}
+		else if (key == 'd')
+		{
+		}
+	}
 	return 0;
 }
 
